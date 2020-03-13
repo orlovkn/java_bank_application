56fcfc6c573572998219dd807f513580db9a8664
Konstantin Orlov <orlovkn.job@gmail.com>
2020-03-13 07:57:04 +0300

    finish

--- src/com/ehoteam/java_bank_application/Main.java
+++ src/com/ehoteam/java_bank_application/Main.java
@@ -2,12 +2,43 @@ package com.ehoteam.java_bank_application;
 
 import java.beans.Customizer;
 import java.io.FileNotFoundException;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
+        List<Account> accounts = new LinkedList<Account> ();
+
+        // read a CSV file and create new accounts based  on that data
         String file = "D:\\java_bank_application\\src\\data\\original.csv";
+        List<String[]> newAccountHolders = util.cav.read (file);
+
+        for (String[] accountHolder : newAccountHolders) {
+            System.out.println ("\nNEW ACCOUNT");
+            String name = accountHolder[0];
+            String sSN = accountHolder[1];
+            String accountType = accountHolder[2];
+            double initDeposit = Double.parseDouble (accountHolder[3]);
+
+            System.out.println (name + " " + sSN + " " + accountType + " $" + initDeposit);
+            if (accountType.equals ("Savings")) {
+                accounts.add (new Savings(name, sSN, initDeposit));
+            } else if (accountType.equals ("Checking")) {
+                accounts.add(new Checking (name, sSN, initDeposit));
+            } else {
+                System.out.println ("ERROR READING ACCOUNT TYPE");
+            }
+        }
+
+//        accounts.get(5).showInfo ();
+        for (Account acc : accounts) {
+            System.out.println ("\n*********************");
+            acc.showInfo ();
+        }
+    }
+
+
 
 
         /*
@@ -18,15 +49,4 @@ public class Main {
         System.out.println ("*************");
         save1.showInfo ();
 */
-        // read a CSV file and create new accounts based  on that data
-        List<String[]> newAccountHolders = util.cav.read (file);
-
-        for (String[] accountHolder : newAccountHolders) {
-            System.out.println ("\nNEW ACCOUNT");
-            System.out.println (accountHolder[0]);
-            System.out.println (accountHolder[1]);
-            System.out.println (accountHolder[2]);
-            System.out.println (accountHolder[3]);
-        }
-    }
 }
